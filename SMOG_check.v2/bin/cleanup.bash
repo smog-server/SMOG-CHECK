c74c5e6018a99afa385c7518dbf022cd868652ee
Paul Whitford <p.whitford@neu.edu>
2015-05-15 12:14:31 -0400

    further cleaned organization

--- SMOG_check.v2/bin/cleanup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm *contacts
-rm *settings
-rm *output
-rm *top
-rm *ndx
-rm shadow.log
-rm -r FAILED.shadow  FAILED.shadow.CA
--- SMOG_check.v2_default/bin/top.clean.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-FILE=$1
-OUT=$2
-grep -v ';' $FILE | grep -v "^ *$" | awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' > $OUT
