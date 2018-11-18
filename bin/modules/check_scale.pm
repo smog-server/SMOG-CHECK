6e16e90e86dfa0594dc46e38440f56e34dd09b99
Paul Whitford <p.whitford@neu.edu>
2018-11-18 18:52:58 -0500

    scale-energies: verifies that unchanged directives are, in fact, unchanged

--- bin/modules/check_scale.pm
+++ bin/modules/check_scale.pm
@@ -16,7 +16,7 @@ sub check_scale
  my $FAILSUM=0;
  my $tool="scale";
  my $printbuffer="";
- my @FAILLIST = ('NON-ZERO EXIT','UNINITIALIZED VARIABLES');
+ my @FAILLIST = ('NON-ZERO EXIT','UNINITIALIZED VARIABLES','UNCHANGED DIRECTIVES');
  %FAIL=resettests(\%FAIL,\@FAILLIST);
 
 # generate an AA model RNA 
@@ -34,13 +34,34 @@ sub check_scale
  `$exec -f AA.tmp.top -n share/PDB.files/sample.AA.ndx -rc 1.5 -rd 1.2 < $pdbdir/in.groups &> output.$tool`;
  ($FAIL{"NON-ZERO EXIT"},$FAIL{"UNINITIALIZED VARIABLES"})=checkoutput("output.$tool");
 
+ # read in original
+ my $string=loadfile("AA.tmp.top");
+ my ($DATA,$DIRLIST)=checkdirectives($string);
+ my %DIRLIST=%{$DIRLIST};
+ my @DATA=@{$DATA};
+ $string=loadfile("smog.rescaled.top");
+ my ($DATA2,$DIRLIST2)=checkdirectives($string);
+ my %DIRLIST2=%{$DIRLIST2};
+ my @DATA2=@{$DATA2};
+ my $samedirs=0; 
+ foreach my $DIR("defaults","atomtypes","moleculetype","atoms","bonds","angles","molecules","exclusions")
+ {
+	$samedirs++;
+	if($DATA[$DIRLIST{"$DIR"}] eq $DATA2[$DIRLIST2{"$DIR"}]){
+	 	$samedirs--;
+	}else{
+		print "issue: directive $DIR changed, but it shouldn\'t\n";
+	}
+ }
+ $FAIL{"UNCHANGED DIRECTIVES"}=$samedirs;
+
 ## add checks here
 #check with different output names
 #verify that if 
  ($FAILED,$printbuffer)=failsum(\%FAIL,\@FAILLIST);
  $FAILSUM += $FAILED;
  if($FAILED !=0){
-  savefailed(1,("output.$tool","smog.rescaled.top"));
+  savefailed(1,("output.$tool","smog.rescaled.top","smog.rescaled.top","AA.tmp.contacts" , "AA.tmp.gro","AA.tmp.ndx", "AA.tmp.top"));
   print "$printbuffer\n";
  }else{
   clearfiles(("output.$tool","smog.rescaled.top","AA.tmp.contacts" , "AA.tmp.gro","AA.tmp.ndx", "AA.tmp.top"));
