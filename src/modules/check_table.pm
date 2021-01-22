ae4dd0359e18fbee59d269dbfd752f81178727dd
Paul <whitford@laptop>
2021-01-21 23:30:39 -0500

    table: -unit and -temp tests

--- src/modules/check_table.pm
+++ src/modules/check_table.pm
@@ -15,6 +15,7 @@ sub check_table
  my $FAILED;
  my $FAILSUM=0;
  my $tool="table";
+ my $name;
  my @FAILLIST = ('NON-ZERO EXIT','CORRECT VALUES');
  %FAIL=resettests(\%FAIL,\@FAILLIST);
 
@@ -32,6 +33,7 @@ sub check_table
  }
 
  %FAIL=resettests(\%FAIL,\@FAILLIST);
+
  print "\tChecking custom table\n"; 
  `$exec -M 10 -n 6 -ic 150 -tl 3 -sd 0.8 -sc 1.2 -table table.2.xvg &> output.$tool`;
  $FAIL{"NON-ZERO EXIT"}=$?;
@@ -43,6 +45,53 @@ sub check_table
  }else{
   clearfiles(("table.2.xvg","output.$tool"));
  }
+
+ %FAIL=resettests(\%FAIL,\@FAILLIST);
+
+ $name="kCal300";
+ print "\tChecking temperature option\n"; 
+ `$exec -temp 300 -table table.$name.xvg &> output.$tool`;
+ $FAIL{"NON-ZERO EXIT"}=$?;
+ $FAIL{"CORRECT VALUES"}=compare_table("table.$name.xvg","share/refs/table.$name.xvg");
+ my ($FAILED,$printbuffer)=failsum(\%FAIL,\@FAILLIST);
+ if($FAILED !=0){
+  savefailed(3,("table.$name.xvg","output.$tool"));
+  print "$printbuffer\n";
+ }else{
+  clearfiles(("table.$name.xvg","output.$tool"));
+ }
+
+ %FAIL=resettests(\%FAIL,\@FAILLIST);
+
+ $name="kJ300";
+ print "\tChecking unit option\n"; 
+ `$exec -unit kJ -table table.$name.xvg &> output.$tool`;
+ $FAIL{"NON-ZERO EXIT"}=$?;
+ $FAIL{"CORRECT VALUES"}=compare_table("table.$name.xvg","share/refs/table.$name.xvg");
+ my ($FAILED,$printbuffer)=failsum(\%FAIL,\@FAILLIST);
+ if($FAILED !=0){
+  savefailed(3,("table.$name.xvg","output.$tool"));
+  print "$printbuffer\n";
+ }else{
+  clearfiles(("table.$name.xvg","output.$tool"));
+ }
+
+ %FAIL=resettests(\%FAIL,\@FAILLIST);
+
+ $name="kJ100";
+ print "\tChecking unit and temp options\n"; 
+ `$exec -temp 100 -unit kJ -table table.$name.xvg &> output.$tool`;
+ $FAIL{"NON-ZERO EXIT"}=$?;
+ $FAIL{"CORRECT VALUES"}=compare_table("table.$name.xvg","share/refs/table.$name.xvg");
+ my ($FAILED,$printbuffer)=failsum(\%FAIL,\@FAILLIST);
+ if($FAILED !=0){
+  savefailed(3,("table.$name.xvg","output.$tool"));
+  print "$printbuffer\n";
+ }else{
+  clearfiles(("table.$name.xvg","output.$tool"));
+ }
+
+
 
  $FAILSUM += $FAILED;
  return ($FAILSUM, $printbuffer);
