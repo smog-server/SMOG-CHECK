08269d241b18e161115bdbe3542e490ffbf11eda
Paul <whitford@laptop>
2021-07-27 14:39:46 -0400

    minor runGMX call fix

--- src/check.v2.pl
+++ src/check.v2.pl
@@ -1152,7 +1152,7 @@ sub smogchecker
   &checktop($openSMOG,$model,$gaussian);
   &finalchecks;
   # if GMX tests are turned on, then run them
-  $FAIL{'GMX COMPATIBLE'}=runGMX($model,$CHECKGMX,$CHECKGMXGAUSSIAN,$GMXEDITCONF,$GMXPATH,$GMXPATHGAUSSIAN,$GMXEXEC,$GMXMDP,$GMXMDPCA,$gaussian,$PDB,$openSMOG,$PDB,$g96);
+  $FAIL{'GMX COMPATIBLE'}=runGMX($model,$CHECKGMX,$CHECKGMXGAUSSIAN,$GMXEDITCONF,$GMXPATH,$GMXPATHGAUSSIAN,$GMXEXEC,$GMXMDP,$GMXMDPCA,$gaussian,$PDB,$openSMOG,$PDB,$g96,"no");
  }else{
   $fail_log .= failed_message("SMOG 2 exited with non-zero exit code when trying to process this PDB file.");
   $FAIL_SYSTEM++;
