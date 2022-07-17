70dfbc0603b2b4b16d94077ae1adcf46acd01a68
Paul <whitford@laptop>
2022-07-17 15:07:00 -0400

    updated for checkall

--- src/scm-test.bash
+++ src/scm-test.bash
@@ -20,52 +20,51 @@ problem=0
 for name in CI2
 do
 
-dir=$TEMPLATE_DIR/$name
-
-for coarse in AA AACA CA
-do
-
-if [ ! -f $dir/$name.$coarse.contacts ]
-then
-echo template files are missing from $TEMPLATE_DIR
-exit
-fi
-
-java -jar $SCM -g $dir/$name.gro -t $dir/$name.top --default -o $name.$coarse.contacts --coarse $coarse &> scm.output
-
-scmfinished=$(grep Finished scm.output | wc | awk '{print $1}')
-if [ $scmfinished -lt 1 ]
-then
-echo SCM.jar didnt finish running. Check that it exists in $SMOG_PATH/src/tool or look at scm.output for run output.
-exit
-fi
-
-if [ ! -f $name.$coarse.contacts ]
-then
-echo SCM.jar did not print an output contact file. Look at scm.output for run output.
-exit
-fi
-
-
-numlines=$(diff $dir/$name.$coarse.contacts $name.$coarse.contacts | wc | awk '{print $1}')
-if [ $numlines -gt 0 ]
-then
-echo option --coarse $coarse differed from template
-problem=1
-fi
-
-rm $name.$coarse.contacts
-
-done
+	dir=$TEMPLATE_DIR/$name
+	
+	for coarse in AA AACA CA
+	do
+		if [ ! -f $dir/$name.$coarse.contacts ]
+		then
+			echo template files are missing from $TEMPLATE_DIR
+			exit 1
+		fi
+		
+		java -jar $SCM -g $dir/$name.gro -t $dir/$name.top --default -o $name.$coarse.contacts --coarse $coarse &> scm.output
+		
+		scmfinished=$(grep Finished scm.output | wc | awk '{print $1}')
+		if [ $scmfinished -lt 1 ]
+		then
+			echo SCM.jar didnt finish running. Check that it exists in $SMOG_PATH/src/tool or look at scm.output for run output.
+			exit 1
+		fi
+		
+		if [ ! -f $name.$coarse.contacts ]
+		then
+			echo SCM.jar did not print an output contact file. Look at scm.output for run output.
+			exit 1
+		fi
+		
+		
+		numlines=$(diff $dir/$name.$coarse.contacts $name.$coarse.contacts | wc | awk '{print $1}')
+		if [ $numlines -gt 0 ]
+		then
+			echo option --coarse $coarse differed from template
+			problem=1
+		fi
+		
+		rm $name.$coarse.contacts
+	done
 done
 
 echo
 if [ $problem -gt 0 ]
 then
-echo There were issues!
+	echo There were issues!
+	exit 1
 else
-echo "PASSED ALL"
-rm scm.output
+	echo "PASSED ALL"
+	rm scm.output
 fi
 
 
